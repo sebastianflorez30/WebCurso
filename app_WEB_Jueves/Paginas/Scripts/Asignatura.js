b4426b598ffc6110628cf27b06e000f4594c7222
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2024-06-08 14:26:51 -0500

    Entrega Final

--- app_WEB_Jueves/Paginas/Scripts/Asignatura.js
+++ app_WEB_Jueves/Paginas/Scripts/Asignatura.js
@@ -1,6 +1,7 @@
 ﻿jQuery(function () {
     //Registrar los botones para responder al evento click
     $("#dvMenu").load("../Paginas/Menu.html")
+    LlenarTablaAsignaturas();
     $("#btnInsertar").on("click", function () {
         EjecutarComando("POST");
     });
@@ -15,6 +16,42 @@
     });
 });
 
+async function LlenarTablaAsignaturas() {
+    //Invoca el método GET, del servicio de tipos de producto
+    //Solo se invoca el fetch
+    try {
+        const Respuesta = await fetch("http://localhost:62586/api/Asignaturas",
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+        //Leer la respuesta del servicio
+        const Resultado = await Respuesta.json();
+        //Presentar a respuesta en el html
+        //El resultado está en formato JSON con la lista de tipos de producto
+        //Se debe recorrer para llenar la tabla
+        //Llena el encabezado
+        var Columnas = [];
+        NombreColumnas = Object.keys(Resultado[0]);
+        for (var i in NombreColumnas) {
+            Columnas.push({
+                data: NombreColumnas[i],
+                title: NombreColumnas[i]
+            });
+        }
+        //Llena los datos
+        $("#tblAsignatura").DataTable({
+            data: Resultado,
+            columns: Columnas,
+            destroy: true
+        });
+    }
+    catch (_error) {
+        //Presentar a respuesta del error en el html
+        $("#dvMensaje").html(_error);
+    }
+}
 async function Consultar() {
     let Codigo = $("#txtCodigo").val();
     //Para el consultar, normalmente no se envía en el body una estructura, sino que se pasan los datos
@@ -48,7 +85,7 @@ async function EjecutarComando(Comando) {
     //Construir el json que se va a enviar al servicio
     let DatosAsignatura = {
         Codigo: Codigo,
-        Nombre: Nombre               
+        NombreAsignatura: Nombre 
     }
     try {
         const Respuesta = await fetch("http://localhost:62586/api/Asignaturas",
